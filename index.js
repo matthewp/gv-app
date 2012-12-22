d3b6ae12d04f188f7dc571dac2d485cec3853e1b
Matthew Phillips <matthew@matthewphillips.info>
2012-12-21 20:08:44 -0500

    Added the commander package.

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var ClientLogin = require('./lib/clientlogin.js').ClientLogin,
     sms = require('./lib/sms.js'),
     fs = require('fs'),
-    ini = require('ini');
+    ini = require('ini'),
+    program = require('commander');
+
+program.version('0.1.0')
+  .option('list', 'List messages')
+  .option('contacts', 'List your contacts')
+  .option('sms [contact] [message]', 'Send an sms to a contact')
+  .parse(process.argv);
 
 var configFile = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'] + '/.gvrc';
-var config = ini.parse(fs.readFileSync(configFile, 'utf-8'));
+var config;
+if(fs.exists(configFile)) {
+  config = ini.parse(fs.readFileSync(configFile, 'utf-8'));
+} else {
+  console.log('No config file exists.');  
+}
 
 //var clientLogin = new ClientLogin(email, password, 'grandcentral', 'HOSTED_OR_GOOGLE');
 
 var log = console.log.bind(console);
-
-log(configFile);
-log(typeof config);
-log(typeof config.email);
 
 /*
 clientLogin.authorize(function(sid, lsid, auth) {
